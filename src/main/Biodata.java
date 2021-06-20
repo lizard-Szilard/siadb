c352867ab5a300a394332ed692c99d3ef025a3f1
User <User@DESKTOP-P1Q1DQN>
2021-06-20 19:26:41 +0700

    change default button enter

---
+++ src/main/Biodata.java
@@ -0,0 +1,59 @@
+
+package main;
+public class Biodata {
+
+	private String id;
+	private String nama;
+	private String alamat;
+	private String pekerjaan;
+	private String phone;
+	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+
+	public String getPekerjaan() {
+		return pekerjaan;
+	}
+
+	public void setPekerjaan(String pekerjaan) {
+		this.pekerjaan = pekerjaan;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String nomorhandphone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/main/FXMLController.java
@@ -0,0 +1,86 @@
+
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+import java.sql.*;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class FXMLController implements Initializable {
+
+	@FXML
+	private TableView<Biodata> biodataTableView;
+	@FXML
+	private TableColumn<?, ?> idTableColumn;
+	@FXML
+	private TableColumn<?, ?> namaTableColumn;
+	@FXML
+	private TableColumn<?, ?> alamatTableColumn;
+	@FXML
+	private TableColumn<?, ?> pekerjaanTableColumn;
+	@FXML
+	private TableColumn<?, ?> nomorHandphoneTableColumn;
+	@FXML
+	private TableColumn<?, ?> emailTableColumn;
+
+	/**
+	 *
+	 * @param url
+	 * @param rb
+	 */
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		TableViewBiodata();
+	}	
+	private void TableViewBiodata() {
+		ObservableList<Biodata> listBiodata = FXCollections.observableArrayList();
+
+		String user = "myMSQLWINDOWS";
+		String passwd = "msqlwindowsapp1";
+		String url = "jdbc:mysql://localhost:3306/siadb";
+
+		try ( Connection connection = DriverManager.getConnection(url, user, passwd)) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM biodata");
+			while (resultSet.next()) {
+				String idTable = resultSet.getString("id");
+				String namaTable = resultSet.getString("nama");
+				String alamatTable = resultSet.getString("alamat");
+				String pekerjaanTable = resultSet.getString("pekerjaan");
+				String nomorHandphoneTable = resultSet.getString("nomorhandphone");
+				String emailTable = resultSet.getString("email");
+
+				
+				Biodata biodata = new Biodata();
+				biodata.setId(idTable);
+				biodata.setNama(namaTable);
+				biodata.setAlamat(alamatTable);
+				biodata.setPekerjaan(pekerjaanTable);
+				biodata.setPhone(nomorHandphoneTable);
+				biodata.setEmail(emailTable);
+
+				listBiodata.add(biodata);
+			}
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+
+		idTableColumn.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		namaTableColumn.setCellValueFactory(new PropertyValueFactory<>("Nama"));
+		alamatTableColumn.setCellValueFactory(new PropertyValueFactory<>("Alamat"));
+		pekerjaanTableColumn.setCellValueFactory(new PropertyValueFactory<>("Pekerjaan"));
+		nomorHandphoneTableColumn.setCellValueFactory(new PropertyValueFactory<>("Nomor Handphone"));
+		emailTableColumn.setCellValueFactory(new PropertyValueFactory<>("Email"));
+
+		biodataTableView.setItems(listBiodata);
+
+	}
+}
